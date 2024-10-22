a7cee99142fdd1d9375ac02560e888b6c7657b5c
Jaswanth <jaswanthdata1@gmail.com>
2024-10-22 16:02:33 +0530

    final

--- frontend/src/components/ProgramListScroll.jsx
+++ frontend/src/components/ProgramListScroll.jsx
@@ -82,7 +82,7 @@ const ProgramListScroll = ({ data, text }) => {
                     </div>
                   </div>
                 </Link>
-                <div className="absolute -bottom-5 z-50 font-sm text-white md:hidden text-center font-light h-[40px] w-full py-1 px-2">
+                <div className="absolute bottom-0 z-50 font-sm text-white md:hidden text-center font-light h-[40px] w-full py-1 px-2">
                   {contentType === "movie" ? record.title : record.name}
                 </div>
               </div>
